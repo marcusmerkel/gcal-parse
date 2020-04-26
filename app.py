32cdfc26f34e8f6530168c5249be8120e9e4b97e
Marcus Merkel <marcus@marcusmerkel.de>
2020-04-26 17:27:30 +0200

    added flexible calendar ID support and UI

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 from __future__ import print_function
 import datetime
 import os.path
-import sys
-import locale
+import pickle
 from datetime import datetime
 from googleapiclient.discovery import build
-from google.oauth2 import service_account
+from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-
 from flask import Flask, flash, jsonify, redirect, render_template, request
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
-SCOPES = ['https://www.googleapis.com/auth/calendar']
-SERVICE_ACCOUNT_FILE = 'private_key/gcal Py html Parser-7ddf822e4fdb.json'
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 # Configure application
 app = Flask(__name__)
@@ -21,29 +18,54 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-# Set Language to Local
-locale.setlocale(locale.LC_ALL, '')
+TEST_CAL_ID = "8mkdbklaaet84afcnf0v4kvjcs@group.calendar.google.com"
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    performer = {}
-    performer["name"] = "Marcus Merkel"
-    performer["givenName"] = "Marcus"
-    performer["familyName"] = "Merkel"
-    return render_template("index.html", eventList=get_dates(20), performer=performer)
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        performer = {}
+        performer["name"] = request.form.get("performerName")
+        if performer["name"] != "":
+            performer["familyName"] = performer["name"].split(" ")[-1]
+            performer["givenName"] = performer["name"].replace(" " + performer["familyName"], "")
+        cal_id = request.form.get("calendarID")
+        max_num = request.form.get("maxNumber")
+        categories = request.form.get("categories").split(", ")
+        return render_template("calendar.html", eventList=get_dates(cal_id, max_num, categories), performer=performer)
+        
 
 # Getting the list of maximum <max_number> events
-def get_dates(max_number):
+def get_dates(calendar_id, max_number, categories):
     # Credentials for Google Calendar API
-    creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    creds = None
 
-    # Building a API object
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
     service = build('calendar', 'v3', credentials=creds)
 
     # Call the Calendar API
     now = datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
     print('Getting the upcoming', max_number, 'events')
-    events_result = service.events().list(calendarId='8mkdbklaaet84afcnf0v4kvjcs@group.calendar.google.com', timeMin=now, maxResults=max_number, singleEvents=True, orderBy='startTime').execute() # pylint: disable=maybe-no-member
+    events_result = service.events().list(calendarId=calendar_id, timeMin=now, maxResults=max_number, singleEvents=True, orderBy='startTime').execute() # pylint: disable=maybe-no-member
     events = events_result.get('items', [])
 
     if not events:
@@ -69,7 +91,7 @@ def get_dates(max_number):
         evnt['end_dttm_iso'] = end
         evnt['start_weekday'] = start_dttm.time().strftime("%A")
         if start_dttm.time().strftime("%H:%M") == "00:00" and end_dttm.time().strftime("%H:%M") == "00:00":
-            evnt['start_time'] = "ganztägig"
+            evnt['start_time'] = "whole day"
             evnt['end_time'] = ""
         else:
             evnt['start_time'] = start_dttm.time().strftime("%H:%M")
@@ -79,47 +101,27 @@ def get_dates(max_number):
 
         if 'description' in event.keys():
             desc = event['description']
-            # replacement not necessary because treating html literally now
-            # desc = desc.replace("<p>", "").replace("</p>", "\n").replace("<br>", "\n")
             evnt['description'] = desc.replace("\n\n\n", "\n\n")
-            if "Operette" in desc:
-                evnt['type'] = "Operette"
-            elif "Musical" in desc:
-                evnt['type'] = "Musical"
-            elif "Konzert" in desc:
-                evnt['type'] = "Konzert"
-            elif "Kammer" in desc:
-                evnt['type'] = "Kammermusik"
-            elif "Lieder" in desc:
-                evnt['type'] = "Lied"
-            elif "Oper" in desc and not "Operette" in desc:
-                evnt['type'] = "Oper"
+            for category in categories:
+                if category in desc:
+                    evnt['type'] = category
         else:
             evnt['description'] = event['summary']
             evnt['type'] = ""
-
-        evnt['opus'] = ""
-        for word in event['summary'].split(" "):
-            if word.isupper():
-                evnt['opus'] = evnt['opus'] + word + " "
 
         if not 'location' in event.keys():
             evnt['location'] = ""
         else:
             loc = event['location']
             evnt['location'] = loc
-            evnt['locationName'] = loc.split(", ", 1)[0]
-            evnt['locationAddress'] = loc.split(", ", 1)[1]
+            if len(loc.split(", ", 1)) > 1:
+                evnt['locationName'] = loc.split(", ", 1)[0]
+                evnt['locationAddress'] = loc.split(", ", 1)[1]
+            else:
+                evnt['locationName'] = loc
+                evnt['locationAddress'] = ""
             evnt['locationMapsSearch'] = "https://google.com/maps/search/" + evnt['locationName'].replace(" ", "+")
 
         eventList.append(evnt)
 
-    for event in eventList:
-        print(event['opus'])
-        print(event['title'])
-
     return eventList
-
-if __name__ == '__main__':
-    app.debug = True
-    app.run(host = 'http://marcus-macbook-pro-16.local/', port = 8888)
